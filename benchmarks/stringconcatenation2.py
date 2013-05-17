3504fd30436e23df6da72cace1e5a2d233a1286e
lregebro <lregebro@d5fba87d-5ff1-455c-97ce-c0e515274c82>
2013-05-17 10:14:58 +0000

    Done

--- benchmarks/stringconcatenation2.py
+++ benchmarks/stringconcatenation2.py
@@ -6,22 +6,30 @@ try:
 except NameError:
     xrange = range
 
-a = "aaaaaaaaaaaaaaaaa"
+strings = ['A' * x for x in xrange(1000)]
 
 def simple_f():
     b = ''
-    for x in range(10000):
-        b = b + a
+    for x in range(999, -1, -1):
+        b = b + strings[x]
 
-simple_b = Benchmark(simple_f, description = "Simple concatenation: s = s1 + s2")
+simple_b = Benchmark(simple_f, description = "Simple concatenation: s1 = s1 + s2")
      
 
 def join_f():
     l = []
-    for x in range(10000):
-        l.append(a)
-    b = ''.join(a)
+    for x in range(999, -1, -1):
+        l.append(strings[x])
+    b = ''.join(l)
 
-join_b = Benchmark(join_f, description = "Joining two strings: s1 = ''.join((s1, s2))")
+join_b = Benchmark(join_f, description = "Joining a list: s1 = ''.join(l)")
 
-suite = Suite([simple_b, join_b])
+
+def extend_f():
+    b = ''
+    for x in range(999, -1, -1):
+        b += strings[x]
+
+extend_b = Benchmark(extend_f, description = "iadd: s1 += s2")
+
+suite = Suite([simple_b, join_b, extend_b])
