35bd6b39641e7ce7fa5eefd7a527969a6920ee60
Lennart Regebro <regebro@gmail.com>
2017-05-16 17:06:15 +0200

    More locals

--- benchmarks/stringconcatenation2.py
+++ benchmarks/stringconcatenation2.py
@@ -8,39 +8,43 @@ except NameError:
 
 uchar = u'A'
 bchar = uchar.encode('ascii')
+unull = u''
+bnull = unull.encode('ascii')
 
 def simple_b_f():
-    s = u''.encode('ascii')
+    s = bnull
+    f = bchar
     for x in xrange(999, -1, -1):
-        f = bchar * x
-        s = s + f
+        s = s + f * x
 
 simple_b_b = Benchmark(simple_b_f, description = "Simple byte concatenation: s1 = s1 + s2")
 
 
 def simple_u_f():
-    s = u''
+    s = unull
+    f = uchar
     for x in xrange(999, -1, -1):
-        f = uchar * x
-        s = s + f
+        s = s + f * x
 
 simple_u_b = Benchmark(simple_u_f, description = "Simple unicode concatenation: s1 = s1 + s2")
 
 def join_b_f():
+    s = bnull
+    f = bchar
     l = []
     for x in xrange(999, -1, -1):
-        l.append(bchar * x)
-    j = u''.encode('ascii')
-    s = j.join(l)
+        l.append(f * x)
+    s = s.join(l)
 
 join_b_b = Benchmark(join_b_f, description = "Joining a byte list: s1 = ''.join(l)")
 
 def join_u_f():
+    s = unull
+    f = uchar
     l = []
     for x in xrange(999, -1, -1):
-        l.append(uchar * x)
-    j = u''
-    s = j.join(l)
+        l.append(f * x)
+    s = s.join(l)
 
 join_u_b = Benchmark(join_u_f, description = "Joining a byte list: s1 = ''.join(l)")
 
--- benchmarks/stringconcatenation5.py
+++ benchmarks/stringconcatenation5.py
@@ -10,33 +10,41 @@ uchar = u'A'
 bchar = uchar.encode('ascii')
 stringies = [uchar * x for x in xrange(1000)]
 byties = [bchar * x for x in xrange(1000)]
+unull = u''
+bnull = unull.encode('ascii')
+
 
 def simple_b_f():
-    s = u''.encode('ascii')
+    thelist = byties
+    s = bnull
     for x in xrange(999, -1, -1):
-        f = byties[x]
+        f = thelist[x]
         s = s + f
 
 simple_b_b = Benchmark(simple_b_f, description = "Simple byte concatenation: s1 = s1 + s2")
 
 
 def simple_u_f():
-    s = u''
+    thelist = stringies
+    s = unull
     for x in xrange(999, -1, -1):
-        f = stringies[x]
+        f = thelist[x]
         s = s + f
 
 simple_u_b = Benchmark(simple_u_f, description = "Simple unicode concatenation: s1 = s1 + s2")
 
 def join_b_f():
-    j = u''.encode('ascii')
-    s = j.join(byties)
+    thelist = byties
+    s = bnull
+    s = s.join(thelist)
 
 join_b_b = Benchmark(join_b_f, description = "Joining a byte list: s1 = ''.join(l)")
 
+
 def join_u_f():
-    j = u''
-    s = j.join(stringies)
+    thelist = stringies
+    s = unull
+    s = s.join(thelist)
 
 join_u_b = Benchmark(join_u_f, description = "Joining a byte list: s1 = ''.join(l)")
 
