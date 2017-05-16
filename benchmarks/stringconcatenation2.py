6a2b68bee6313d7965706869f0c0ead56ef61903
Lennart Regebro <regebro@gmail.com>
2017-05-16 17:22:39 +0200

    small change

--- benchmarks/stringconcatenation2.py
+++ benchmarks/stringconcatenation2.py
@@ -13,37 +13,37 @@ bnull = unull.encode('ascii')
 
 def simple_b_f():
     s = bnull
-    f = bchar
+    f = bchar * 500
     for x in xrange(999, -1, -1):
-        s = s + f * x
+        s = s + f
 
 simple_b_b = Benchmark(simple_b_f, description = "Simple byte concatenation: s1 = s1 + s2")
 
 
 def simple_u_f():
     s = unull
-    f = uchar
+    f = uchar * 500
     for x in xrange(999, -1, -1):
-        s = s + f * x
+        s = s + f
 
 simple_u_b = Benchmark(simple_u_f, description = "Simple unicode concatenation: s1 = s1 + s2")
 
 def join_b_f():
     s = bnull
-    f = bchar
+    f = bchar * 500
     l = []
     for x in xrange(999, -1, -1):
-        l.append(f * x)
+        l.append(f)
     s = s.join(l)
 
 join_b_b = Benchmark(join_b_f, description = "Joining a byte list: s1 = ''.join(l)")
 
 def join_u_f():
     s = unull
-    f = uchar
+    f = uchar * 500
     l = []
     for x in xrange(999, -1, -1):
-        l.append(f * x)
+        l.append(f)
     s = s.join(l)
 
 join_u_b = Benchmark(join_u_f, description = "Joining a byte list: s1 = ''.join(l)")
